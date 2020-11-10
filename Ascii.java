1eaf3270bb4553a90a886e76a2e0580205d46cea
Bavithra-P <74226336+Bavithra-P@users.noreply.github.com>
2020-11-10 10:07:53 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Ascii {
+	 public char alphabet;
+	int convert;
+	
+	 public void conversion() {
+		 convert=(int)alphabet;
+		 System.out.println(" The ASCII value of " + alphabet + " is " + convert);
+		
+	}
+
+}
