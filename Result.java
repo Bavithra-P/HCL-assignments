0dc4ea701aa49f0da658c1a8ea0ca8c30e571d90
Bavithra-P <74226336+Bavithra-P@users.noreply.github.com>
2020-11-11 15:04:14 +0530

    Add files via upload

---
+++ Result.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Result {
+	public int num1;
+	public int num2;
+	public int num3;
+
+	public void calculate() {
+		int Result1 = -num1 + num2 * num3;
+		System.out.println("The Result is:" + Result1);
+	}
+
+}
