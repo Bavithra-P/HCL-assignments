37dfeefab0f6ce16fc4d6adc7ed70859fa1300c5
Bavithra-P <74226336+Bavithra-P@users.noreply.github.com>
2020-11-11 15:04:55 +0530

    Add files via upload

---
+++ ResultMain.java
@@ -0,0 +1,15 @@
+package service;
+
+import main.Result;
+
+public class ResultMain {
+
+	public static void main(String[] args) {
+		Result result = new Result();
+		result.num1 = 5;
+		result.num2 = 8;
+		result.num3 = 6;
+		result.calculate();
+	}
+
+}
